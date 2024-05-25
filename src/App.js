fe4383f7d6f6215c7f34e38f15e46599f9d9c285
sanojsudo <sanoj.kumar@thoughtworks.com>
2024-05-26 01:08:26 +0530

    react app cicd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Integration Ninjas
+          Welcome to New World Sanoj
         </p>
         <a
           className="App-link"
